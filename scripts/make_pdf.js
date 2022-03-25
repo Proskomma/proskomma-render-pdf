7394dffc129c890eed29ee956b36cb73695a8744
Mark Howe <mvahowe@gmail.com>
2022-03-25 13:11:31 +0100

    Neater output; optional pjs script

--- scripts/make_pdf.js
+++ scripts/make_pdf.js
@@ -40,6 +40,7 @@ if (!pdfOutputPath) {
     throw new Error("USAGE: node make_pdf.js <configPath> <pdfOutputPath>");
 }
 config.bookOutput = {};
+config.pjsCallbacks = true; // NEEDED FOR PUPPETEER MONITORING
 
 let ts = Date.now();
 let nBooks = 0;
@@ -47,6 +48,7 @@ let nPeriphs = 0;
 
 const pk = new Proskomma();
 const fqSourceDir = path.resolve(config.configRoot, config.sourceDir);
+console.log(`Loading data from '${fqSourceDir}'`);
 for (const filePath of fse.readdirSync(fqSourceDir)) {
     if (bookMatches(filePath)) {
         console.log(`   ${filePath} (book)`);
@@ -70,28 +72,39 @@ for (const filePath of fse.readdirSync(fqSourceDir)) {
         );
     }
 }
-console.log(`${nBooks} book(s) and ${nPeriphs} peripheral(s) loaded in ${(Date.now() - ts) / 1000} sec`);
-ts = Date.now();
+console.log(`   ${nBooks} book(s) and ${nPeriphs} peripheral(s) loaded in ${(Date.now() - ts) / 1000} sec`);
 
 const config2 = await doRender(pk, config);
 const prePagedJSHtml = config2.output;
 
 const prePagedJSHtmlOutputPath = pdfOutputPath.replace(/\.pdf$/, "")+"_pre_pagedjs.html";
 fse.writeFileSync(prePagedJSHtmlOutputPath, prePagedJSHtml);
-console.log("Pre-PagedJS HTML written to: "+prePagedJSHtmlOutputPath+" (file://"+encodeURI(prePagedJSHtmlOutputPath)+"). View in browser to see rendered HTML with PagedJS.");
-
+console.log(`Pre-PagedJS HTML written to '${prePagedJSHtmlOutputPath}' (file://${encodeURI(prePagedJSHtmlOutputPath)})`);
+console.log("View in browser to see HTML rendered with PagedJS");
+console.log("Rendering HTML using Puppeteer");
+ts = Date.now();
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 await page.setContent(prePagedJSHtml);
 page.on('console', msg => {
-    for (let i = 0; i < msg.args().length; ++i)
-      console.log(`${i}: ${msg.args()[i]}`);
+    for (const arg of msg.args()) {
+        console.log(`   ${arg.toString().split(':')[1] || arg.toString()}`);
+    }
   });
 
 try {
-    console.log("Waiting for Paged.JS to render the TOC...");
-    await page.waitForFunction(() => {console.log("Current Page: "+pjCurrentPageNum);return pjRenderingDone;},
-        {timeout: 120000, polling: "mutation"} // Set timeout here. This is 2 minutes. TODO: Add to config file?
+    let currentPage = {n: 0};
+    await page.waitForFunction(
+    (currentPage) => {
+        if (pjCurrentPageNum !== currentPage.n) {
+            console.log(`Page ${pjCurrentPageNum}`);
+            currentPage.n = pjCurrentPageNum;
+        }
+        return pjRenderingDone;
+    }
+,
+        {timeout: 120000, polling: "mutation"},
+        currentPage
     );
 } catch(e) {
     if (e instanceof puppeteer.errors.TimeoutError) {
@@ -101,14 +114,16 @@ try {
     }
     exit(1);
 }
+console.log(`HTML rendered in ${(Date.now() - ts) /  1000} seconds`)
 
 let staticHtml = await page.content();
 
 const staticHtmlOutputPath = pdfOutputPath.replace(/\.pdf$/, "")+"_static.html";
 fse.writeFileSync(staticHtmlOutputPath, staticHtml);
-console.log("Static HTML written to: "+staticHtmlOutputPath+" (file://"+encodeURI(staticHtmlOutputPath)+")");
-
+console.log(`Static HTML written to '${staticHtmlOutputPath}' (file://${encodeURI(staticHtmlOutputPath)})`);
+console.log("Converting HTML to PDF");
+ts = Date.now();
 await page.pdf({path: pdfOutputPath, format: 'A4'})
-console.log("PDF written to: "+pdfOutputPath)
+console.log(`PDF produced in ${(Date.now() - ts) /  1000} seconds and written to '${pdfOutputPath}'`);
 
 await browser.close();
